2f37007c6a05fcb6aef4c4c95adefa2c22ad87bf
Keïsha Alexander <kea.the.artist101@gmail.com>
2026-06-19 15:02:53 +0200

    FIX views folder path

--- netlify/functions/app.js
+++ netlify/functions/app.js
@@ -1,10 +1,12 @@
 const serverless = require('serverless-http');
 const express = require('express');
+const path = require('path');
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+app.set('views', path.join(__dirname, '../../views'));
 app.set('view engine', 'html');
 app.engine('html', require('ejs').renderFile);
 
