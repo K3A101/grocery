2e7bad2bf9d4f4e8e8613661294db7486a4ed956
Keïsha Alexander <kea.the.artist101@gmail.com>
2026-06-19 18:10:54 +0200

    fix: bundle views folder with netlify function

--- netlify/functions/app.js
+++ netlify/functions/app.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.set('views', path.join(__dirname, '../views'));
+app.set('views', path.join(process.cwd(), 'views'));
 app.set('view engine', 'html');
 app.engine('html', require('ejs').renderFile);
 
