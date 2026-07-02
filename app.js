fb5143067f24c9fe85acc9f2ef548613000ed196
Keïsha Alexander <kea.the.artist101@gmail.com>
2026-07-02 19:13:04 +0200

    FIX

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 require('dotenv').config();
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const supabase = require('./supabase/supabase-config');
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(express.static('static'));
-app.use('/node_modules', express.static('node_modules'));
+app.use(express.static(path.join(__dirname, 'static')));
+app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')));
 
 
 
 app.set('view engine', 'html');
 app.engine('html', require('ejs').renderFile);
-app.set("views", "./views");
+app.set("views", path.join(__dirname, 'views'));
 // app.set('view engine', 'ejs');
 
 
